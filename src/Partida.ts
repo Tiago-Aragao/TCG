a6be9528727cefd976d29a81eebc24b09f55d5e1
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-09-18 17:14:17 -0300

    Feat: Dividindo a resposabilidade das fases de um turno em metodos mais modulares e legiveis iniciarPartida, executarFasePreparacao entre outros. Mesmo alguns sem implementação já faço a estrutura do que está por vim e a separação de como cada fase do turno irá funcionar

--- src/Partida.ts
+++ src/Partida.ts
@@ -16,47 +16,102 @@ export class Partida {
     public readonly jogador1: Jogador;
     public readonly jogador2: Jogador;
     public turnoAtual: number;
+    // Atributos para gerenciar Mulligans:
+    private mulligansRealizados: Set<Jogador> = new Set();
+    private aberturaEncerrada: boolean = false; // prevenir que o mulligan ocorra no turno 1 em vez de antes do primiero turno.
+    // Controle de inicio de Partida:
+    private partidaIniciada: boolean = false;
 
     constructor(jogador1: Jogador, jogador2: Jogador) {
         this.jogador1 = jogador1;
         this.jogador2 = jogador2;
         this.turnoAtual = 1;
     }
+    
+    public iniciarPartida(): void {
+        if (this.partidaIniciada) {
+            return;
+        }
+
+        this.jogador1.comprarMaoInicial();
+        this.jogador2.comprarMaoInicial();
+
+        this.partidaIniciada = true;
+    }
 
     public executarPartida(): Jogador | null {
+        if (!this.partidaIniciada) {
+            this.iniciarPartida();
+        }
+        
+        // Aqui futuramente vira o solicitarMulligan(Jogador); caso algum queira mulligar
+
         while (this.jogador1.estaVivo() && this.jogador2.estaVivo() && this.turnoAtual <= 100) {
             this.executarProximoTurno();
         }
-        if (!this.jogador1.estaVivo()) {
-            return this.jogador2;
-        }
-        if (!this.jogador2.estaVivo()) {
-            return this.jogador1;
-        }
-        return null;
         
-    }
-
-    private podeAtacarJogador(): boolean {
-        // Jogador1 não pode mais atacar o Jogador2 no turno 1:
-        return this.turnoAtual > 1;
+        if (!this.jogador1.estaVivo()) return this.jogador2;
+        if (!this.jogador2.estaVivo()) return this.jogador1;
+        return null;
     }
     
     public executarProximoTurno(): void {
-        console.log(`Turno: ${this.turnoAtual}`);
-        
-        const atacante = (this.turnoAtual % 2 !== 0) ? this.jogador1 : this.jogador2;
-        const defensor = (this.turnoAtual % 2 !== 0) ? this.jogador2 : this.jogador1;
+        if (!this.partidaIniciada) {
+            this.iniciarPartida();
+        }
 
+        console.log(`--- Turno: ${this.turnoAtual} ---`);
+
+        if (!this.aberturaEncerrada) {
+            this.aberturaEncerrada = true;
+        }
+
+        const atacante = (this.turnoAtual % 2 !== 0)? this.jogador1:this.jogador2;
+        const defensor = (this.turnoAtual % 2 !== 0)? this.jogador2:this.jogador1;
+
+        this.executarFasePreparacao(atacante);
+        this.executarFaseCompra(atacante);
+        this.executarFasePrincipal(atacante);
+        this.executarFaseCombate(atacante, defensor);
+        this.executarFaseFinal();
+    }
+
+    private executarFasePreparacao(jogador: Jogador): void {
+        // Futuramente resolver efeitos de fase de preparação.
+    }
+
+    private executarFaseCompra(jogador: Jogador): void {
+        // Jogador 1 não compra no primeiro turno:
+        if (this.turnoAtual > 1) {
+            jogador.comprarCarta();
+        }
+    }
+
+    public solicitarMulligan(jogador: Jogador): boolean {
+        if (!this.partidaIniciada || this.aberturaEncerrada || this.mulligansRealizados.has(jogador)) {
+            console.log(`Mulligan negado para ${jogador.nome}.`);
+            return false;
+        }
+
+        console.log(`${jogador.nome} realizou o Mulligan.`);
+        jogador.fazerMulliganEuropeu();
+
+        this.mulligansRealizados.add(jogador);
+        return true;
+    }
+
+    private executarFasePrincipal(jogador: Jogador): void {
+        // Futuramenet gastar mana, baixar criaturas, usar magias etc.
+    }
+
+    private executarFaseCombate(atacante: Jogador, defensor: Jogador): void {
         for (const linha of ORDEM_DAS_LINHAS) {
             for (const coluna of ORDEM_DAS_COLUNAS) {
                 const criaturaAtacante = atacante.tabuleiro.obterCriatura(linha, coluna);
                 
                 if (criaturaAtacante != null && criaturaAtacante.estaVivo()) {
                     
-                    // Verifica se a criatura está na linha certa para o seu tipo de ataque
                     if (this.podeAtacar(criaturaAtacante, linha)) {
-                        
                         const alvoEncontrado = this.encontrarAlvo(coluna, defensor);
                         
                         if (alvoEncontrado != null) {
@@ -67,18 +122,13 @@ export class Partida {
                                 defensor
                             );
                         }
-                        // CAMPO VAZIO: Tentativa de Ataque Direto
                         else if (!defensor.tabuleiro.possuiCriaturasNoTabuleiro()) {
-                            
-                            // A única regra que impede o ataque direto de QUALQUER criatura é o Turno 1
                             if (this.podeAtacarJogador()) {
                                 console.log(`${criaturaAtacante.nome} atacou o ${defensor.nome} DIRETAMENTE com ${criaturaAtacante.ataque} de dano!`);
                                 defensor.receberDano(criaturaAtacante.ataque);
-                                console.log(`${defensor.nome} | ${defensor.vidaAtual}/${defensor.vidaMaxima}`);
                             } else {
                                 console.log(`${criaturaAtacante.nome} não pode atacar o jogador diretamente no Turno 1.`);
                             }
-                            
                         }
                     } else {
                         console.log(`${criaturaAtacante.nome} não pode atacar na linha: ${linha}`);
@@ -86,13 +136,21 @@ export class Partida {
                 }
             }
         }
-        this.avancarTurno();
+    }
+
+    private executarFaseFinal(): void {
+        // Futuramente realizar operações para encerrar ou ativar efeitos que dependem dessa fase.
+        this.turnoAtual++;
+    }
+
+    
+    private podeAtacarJogador(): boolean {
+        return this.turnoAtual > 1;
     }
 
     private resolverCombate(atacante: Criatura, alvo: Criatura, posicaoAlvo: Posicao, defensor: Jogador): void {
-        console.log(`${atacante.nome} ataca ${alvo.nome} com ${atacante.ataque} de ataque!`);
+        console.log(`${atacante.nome} ataca ${alvo.nome} com ${atacante.ataque} de dano!`);
         alvo.receberDano(atacante.ataque);
-        console.log(`${alvo.nome} está com ${alvo.vida} de vida!`);
         
         if (!alvo.estaVivo()) {
             console.log(`${alvo.nome} foi destruída!`);
@@ -101,19 +159,16 @@ export class Partida {
     }
 
     private encontrarAlvo(colunaOrigem: Coluna, defensor: Jogador): Alvo | null {
-        
         for (const linha of ORDEM_DAS_LINHAS) {
             let alvoMaisProximo: Criatura | null = null;
             let colunaDoAlvo: Coluna | null = null;
-            let menorDistancia = Infinity; // Só para primeira comparação ser sempre verdadeira.
+            let menorDistancia = Infinity;
 
             for (const coluna of ORDEM_DAS_COLUNAS) {
                 const criaturaDefensora = defensor.tabuleiro.obterCriatura(linha, coluna);
                 
                 if (criaturaDefensora != null && criaturaDefensora.estaVivo()) {
-                    // Verifico a distancia:
                     const distancia = Math.abs(colunaOrigem - coluna);
-                    // Atualizo os dados:
                     if (distancia < menorDistancia) {
                         alvoMaisProximo = criaturaDefensora;
                         colunaDoAlvo = coluna;
@@ -122,34 +177,19 @@ export class Partida {
                 }
             }
 
-            // Se achou alguém nesta linha, retorna imediatamente:
             if (alvoMaisProximo != null && colunaDoAlvo != null) {
-                return {
-                    criatura: alvoMaisProximo,
-                    posicao: { linha, coluna: colunaDoAlvo }
-                };
+                return { criatura: alvoMaisProximo, posicao: { linha, coluna: colunaDoAlvo } };
             }
         }
-        // Campo vazio:
         return null;
     }
 
     private podeAtacar(possivelAtacante: Criatura, linha: Linha): boolean {
         switch(possivelAtacante.tipoAtaque) {
-            case 'corpo-a-corpo':
-                return linha === 'frente';
-            case "distancia":
-                return linha === 'fundo';
-            case "magico":
-                return true;
-            // Tratamento de erro por qualquer bug a criatura não ataca:
-            default:
-                return false;
+            case 'corpo-a-corpo': return linha === 'frente';
+            case "distancia": return linha === 'fundo';
+            case "magico": return true;
+            default: return false;
         }
     }
-    
-    private avancarTurno(): void {
-        this.turnoAtual++;
-    }
-
 }
