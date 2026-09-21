bf9a19acd2a63ea3cd2b306302af012e088164d4
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-09-21 16:48:39 -0300

    Feat: Implementação de jogarCartaMao e possuiManaSuficiente.

--- src/Jogador.ts
+++ src/Jogador.ts
@@ -37,22 +37,41 @@ export class Jogador {
         this.mana = Math.min(6, Math.ceil(turnoAtual/2));
     }
 
-    public gastarMana(gasto:number): boolean {
-        if (gasto > this.mana) return false;
-        if (gasto < 0) return false;
-        this.mana -= gasto;
+    public gastarMana(custo:number): boolean {
+        if (custo > this.mana) return false;
+        if (custo < 0) return false;
+        this.mana -= custo;
         return true; // consegui gastar a mana amigos.
+    }
+    // Operação de consulta que será "auxiliar" do jogarCarta:
+    public possuiManaSuficiente(custo: number): boolean {
+        /**
+         * Verifico o custo de mana sem precisar gastar a mana.
+         */
+        return custo >= 0 && custo <= this.mana;
     }
 
     // Para fase de preparação:
-    public jogarCarta(qualCarta: CartaCriatura, posicao: Posicao): boolean {
-        const custoCarta = qualCarta.custoMana;
-        if (this.gastarMana(custoCarta)) {
-            const criatura = qualCarta.criarCriatura();
-            this.tabuleiro.conjurarCriatura(criatura, posicao.linha, posicao.coluna);
-            return true;
+    public jogarCartaMao(qualCarta: CartaCriatura, posicao: Posicao): boolean {
+        // Verifico se a carta existe na mão:
+        if (!this.mao.includes(qualCarta)) {
+            return false;
         }
-        return false;
+        // Verifico se tenho mana suficiente:
+        if (!this.possuiManaSuficiente(qualCarta.custoMana)) {
+            return false;
+        }
+        //
+        if (!this.tabuleiro.podeConjurarCriatura(posicao.linha, posicao.coluna)) {
+            return false;
+        }
+        const criatura = qualCarta.criarCriatura();
+        this.tabuleiro.conjurarCriatura(criatura, posicao.linha, posicao.coluna);
+        this.gastarMana(qualCarta.custoMana);
+        // Agora removo ele da mão:
+        const indiceCarta = this.mao.findIndex(carta => carta === qualCarta);
+        this.mao.splice(indiceCarta, 1);
+        return true;
     }
 
     // Compras de Cartas:
