baed104102c95ae8e623ef3d90983a335307723b
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-09-24 15:20:33 -0300

    Feat: Implementação de EstadoDaPartida como fonte da verdade das fases da mesma e retirada dos bools como fonte de verdade dos estados.

--- src/Partida.ts
+++ src/Partida.ts
@@ -3,57 +3,75 @@ import { Criatura } from "./Criatura";
 import { CartaCriatura } from "./CartaCriatura";
 import { Linha, Coluna, Posicao } from "./Tabuleiro";
 
-// Constantes para acabar com redundancias:
 const ORDEM_DAS_LINHAS: Linha[] = ["frente", "fundo"] as const;
 const ORDEM_DAS_COLUNAS: Coluna[] = [Coluna.Esquerda, Coluna.Meio, Coluna.Direita] as const;
 
-// Melhorar a legibilidade criando um alvo:
+// Famosos Types:
 export type Alvo = {
     criatura: Criatura,
     posicao: Posicao
 }
-
 export type MensagemParaTratamentoErro =
-    "Sucesso" |
-    "ManaInsuficiente" |
-    "CartaNaoEstaNaMao" |
-    "PosicaoOcupada" |
-    "ForaDaFasePrincipal" |
-    "JogadorNaoAtivo" |
-    "ForaDoTurno";
+    | "Sucesso"
+    | "ManaInsuficiente"
+    | "CartaNaoEstaNaMao"
+    | "PosicaoOcupada"
+    | "ForaDaFasePrincipal"
+    | "JogadorNaoAtivo"
+    | "ForaDoTurno";
 export type EstadoDaPartida =
     | "NaoIniciada"
     | "Abertura"
     | "FasePrincipal"
     | "AguardandoTurno";
 
+// Classe Partida:
 export class Partida {
     
     public readonly jogador1: Jogador;
     public readonly jogador2: Jogador;
     public turnoAtual: number;
-    // Atributos para gerenciar Mulligans:
-    private mulligansRealizados: Set<Jogador> = new Set();
-    // Controle estados simples:
-    private aberturaEncerrada: boolean = false; // prevenir que o mulligan ocorra no turno 1 em vez de antes do primiero turno.
-    private partidaIniciada: boolean = false;
-    // Controle de estados da partida:
+    
     private estadoPartida: EstadoDaPartida = "NaoIniciada";
-
+    private mulligansRealizados: Set<Jogador> = new Set();
+    
+    // Construtor da Classe:
     constructor(jogador1: Jogador, jogador2: Jogador) {
         this.jogador1 = jogador1;
         this.jogador2 = jogador2;
         this.turnoAtual = 1;
     }
 
-    // Getter:
+
+    // Meu getters e leitores de estado:
     public get estado(): EstadoDaPartida {
         return this.estadoPartida;
     }
 
-    // Juiz do Mulligan:
+    public obterJogadorAtivo(): Jogador {
+        return (this.turnoAtual % 2 !== 0) ? this.jogador1 : this.jogador2;
+    }
+
+    public obterJogadorDefensor(): Jogador {
+        return (this.turnoAtual % 2 !== 0) ? this.jogador2 : this.jogador1;
+    }
+
+    // Metodos publicos que serão chamados pela API:
+    public iniciarPartida(): void {
+        // Travo varias chamadas:
+        if (this.estadoPartida !== 'NaoIniciada') {
+            return;
+        }
+        this.jogador1.comprarMaoInicial();
+        this.jogador2.comprarMaoInicial();
+        this.estadoPartida = 'Abertura';
+    }
+
     public solicitarMulligan(jogador: Jogador): boolean {
-        if (!this.partidaIniciada || this.aberturaEncerrada || this.mulligansRealizados.has(jogador)) {
+        // Valido se o jogador está na partida (apenas segurança para evitar bugs):
+        const participaDaPartida = jogador === this.jogador1 || jogador === this.jogador2;
+
+        if (!participaDaPartida || this.estadoPartida !== "Abertura" || this.mulligansRealizados.has(jogador)) {
             console.log(`Mulligan negado para ${jogador.nome}.`);
             return false;
         }
@@ -63,23 +81,64 @@ export class Partida {
 
         this.mulligansRealizados.add(jogador);
         return true;
-    }
-    
-    public iniciarPartida(): void {
-        if (this.partidaIniciada) {
-            return;
-        }
-        this.jogador1.comprarMaoInicial();
-        this.jogador2.comprarMaoInicial();
-        this.partidaIniciada = true;
-    }
+}
 
-    public executarPartida(): Jogador | null {
-        
-        if (!this.partidaIniciada) {
+    public iniciarTurno(): void {
+        // Agora uso o meu state para regular o que antes era um bool:
+        if (this.estadoPartida === 'NaoIniciada') {
             this.iniciarPartida();
         }
-        // Aqui futuramente vira o solicitarMulligan(Jogador); caso algum queira mulligar
+
+        // Verifico aqui em vez dentro do if deixando mais legivel:
+        const podeIniciarTurno = (this.estadoPartida === "Abertura" || this.estadoPartida === "AguardandoTurno");
+        if (!podeIniciarTurno) {
+            return;
+        }
+        // Se não eu pego o jgoador ativo:
+        const jogadorAtivo = this.obterJogadorAtivo();
+        // Gero mana:
+        this.executarFasePreparacao(jogadorAtivo); // chama o jogador.gerarManaTurno
+        this.executarFaseCompra(jogadorAtivo);
+        // Iniciando a fase principal:
+        this.estadoPartida = "FasePrincipal";
+    }
+
+    public tentarJogarCarta(cartaEscolhida: CartaCriatura, posicao: Posicao): boolean {
+        /**
+         * Só pode ocorrer na fasePrincipal.
+         * Deve permitir diversas chamadas para o Jogador jogar quantas cartas quiser e tiver mana para jogar.
+         * Por enquanto retorna um bool mas posteriormente vai retornar um type criado por mim exclusivo para o tratamento de erro
+        adequado.
+         */
+        // Verifico que só pode funcionar na FasePrincipal:
+        if (this.estadoPartida !== "FasePrincipal") {
+            // Meu proximo passo é isso aqui retornar uma mensagem de erro especial no futuro. Mas por enquanto o false da
+            // para o gasto hihi:
+            return false;
+        }
+        const jogadorAtivo = this.obterJogadorAtivo();
+        return jogadorAtivo.jogarCartaMao(cartaEscolhida, posicao);
+    }
+
+    public passarTurno(): void {
+        if (this.estadoPartida !== 'FasePrincipal') {
+            // Faço nada impedindo de usar passar turno mais de uma vez:
+            return;
+        }
+        // Caso seja aqui eu executo as etapas normalmente:
+        // Selecionando atacante e defensor:
+        const jogadorAtacante = this.obterJogadorAtivo();
+        const jogadorDefensor = this.obterJogadorDefensor();
+        // Combate:
+        this.executarFaseCombate(jogadorAtacante, jogadorDefensor);
+        // Mudança de turno:
+        this.executarFaseFinal(); // Aqui faz o this.turnoAtual++;
+        // Mudança de estado:
+        this.estadoPartida = 'AguardandoTurno';
+    }
+
+    // Meus loops automaticos:
+    public executarPartida(): Jogador | null {
         while (this.jogador1.estaVivo() && this.jogador2.estaVivo() && this.turnoAtual <= 100) {
             this.executarProximoTurno();
         }
@@ -87,31 +146,42 @@ export class Partida {
         if (!this.jogador2.estaVivo()) return this.jogador1;
         return null;
     }
-    
-    // Primeira Fase:
+
+    public executarProximoTurno(): void {
+        // Protegido contra ser chamado em uma fase principal já aberta:
+        if (this.estadoPartida === 'FasePrincipal') {
+            return;
+        }
+        console.log(`--- Turno: ${this.turnoAtual} ---`);
+
+        this.iniciarTurno();
+
+        const atacante = this.obterJogadorAtivo();
+        this.executarFasePrincipal(atacante);
+        this.passarTurno();
+    }
+
+    // Motores internos e completamente privados:
     private executarFasePreparacao(jogador: Jogador): void {
-        // Na fase de preparação o jogador recebe a mana do turno:
         jogador.gerarManaTurno(this.turnoAtual);
     }
-    // Segunda Fase:
+
     private executarFaseCompra(jogador: Jogador): void {
-        // Jogador 1 não compra no primeiro turno:
         if (this.turnoAtual > 1) {
             jogador.comprarCarta();
         }
     }
-    // Terceira fase (Parte principal onde o jogador toma decisões):
+
     private executarFasePrincipal(jogador: Jogador): void {
-        // Futuramenet gastar mana, baixar criaturas, usar magias etc.
+        // Futuramente gastar mana, baixar criaturas, usar magias etc.
     }
-    // Quarta Fase (Combate automatico):
+
     private executarFaseCombate(atacante: Jogador, defensor: Jogador): void {
         for (const linha of ORDEM_DAS_LINHAS) {
             for (const coluna of ORDEM_DAS_COLUNAS) {
                 const criaturaAtacante = atacante.tabuleiro.obterCriatura(linha, coluna);
                 
                 if (criaturaAtacante != null && criaturaAtacante.estaVivo()) {
-                    
                     if (this.podeAtacar(criaturaAtacante, linha)) {
                         const alvoEncontrado = this.encontrarAlvo(coluna, defensor);
                         
@@ -133,7 +203,12 @@ export class Partida {
             }
         }
     }
-    // Metodos auxiliares de combate:
+
+    private executarFaseFinal(): void {
+        this.turnoAtual++;
+    }
+
+    // Auxiliares para o combate:
     private resolverCombate(atacante: Criatura, alvo: Criatura, posicaoAlvo: Posicao, defensor: Jogador): void {
         console.log(`${atacante.nome} ataca ${alvo.nome} com ${atacante.ataque} de dano!`);
         alvo.receberDano(atacante.ataque);
@@ -143,9 +218,11 @@ export class Partida {
             defensor.tabuleiro.removerCriatura(posicaoAlvo.linha, posicaoAlvo.coluna);
         }
     }
+
     private podeAtacarJogador(): boolean {
         return this.turnoAtual > 1;
     }
+
     private encontrarAlvo(colunaOrigem: Coluna, defensor: Jogador): Alvo | null {
         for (const linha of ORDEM_DAS_LINHAS) {
             let alvoMaisProximo: Criatura | null = null;
@@ -171,6 +248,7 @@ export class Partida {
         }
         return null;
     }
+
     private podeAtacar(possivelAtacante: Criatura, linha: Linha): boolean {
         switch(possivelAtacante.tipoAtaque) {
             case 'corpo-a-corpo': return linha === 'frente';
@@ -178,69 +256,5 @@ export class Partida {
             case "magico": return true;
             default: return false;
         }
-    }
-
-    // Quinta fase (Encerramento):
-    private executarFaseFinal(): void {
-        // Futuramente realizar operações para encerrar ou ativar efeitos que dependem dessa fase.
-        this.turnoAtual++;
-    }
-
-    // Saber quem é o JOGADOR da vez:
-    public obterJogadorAtivo(): Jogador {
-        return (this.turnoAtual % 2 !== 0)? this.jogador1:this.jogador2;
-    }
-    public obterJogadorDefensor(): Jogador {
-        return (this.turnoAtual % 2 !== 0)? this.jogador2:this.jogador1;
-    }
-    
-    // Orquestrador das fases:
-    public executarProximoTurno(): void {
-        if (!this.partidaIniciada) {
-            this.iniciarPartida();
-        }
-
-        console.log(`--- Turno: ${this.turnoAtual} ---`);
-
-        if (!this.aberturaEncerrada) {
-            this.aberturaEncerrada = true;
-        }
-
-        const atacante = this.obterJogadorAtivo();
-        const defensor = this.obterJogadorDefensor();
-
-        this.executarFasePreparacao(atacante);
-        this.executarFaseCompra(atacante);
-        this.executarFasePrincipal(atacante);
-        this.executarFaseCombate(atacante, defensor);
-        this.executarFaseFinal();
-    }
-
-    // Modularização do turno:
-    public iniciarTurno(): void {
-        const jogadorAtivo = this.obterJogadorAtivo();
-        
-        if (!this.partidaIniciada) {
-            this.iniciarPartida();
-        }
-
-        if (!this.aberturaEncerrada) {
-            this.aberturaEncerrada = true;
-        }
-
-        this.executarFasePreparacao(jogadorAtivo);
-        this.executarFaseCompra(jogadorAtivo);
-    }
-
-    public tentarJogarCarta(cartaEscolhida: CartaCriatura, posicao: Posicao): boolean {
-        const jogadorAtivo = this.obterJogadorAtivo();
-        return jogadorAtivo.jogarCartaMao(cartaEscolhida, posicao);
-    }
-
-    public passarTurno(): void {
-        const jogadorAtacante = this.obterJogadorAtivo();
-        const jogadorDefensor = this.obterJogadorDefensor();
-        this.executarFaseCombate(jogadorAtacante, jogadorDefensor);
-        this.executarFaseFinal();
     }
 }
